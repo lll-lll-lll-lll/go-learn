28fd74d849e58285660a3ec5ea75c11c50b6481c
ShunpeIII <63499912+lll-lll-lll-lll@users.noreply.github.com>
2024-01-17 13:20:00 +0000

    fix:

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,9 +13,24 @@ func download(u string) {
 
 func main() {
 	before := time.Now()
+	// goroutineの生成量を10個に抑えることで、サーバへの負荷を抑える
+	limit := make(chan struct{}, 10)
+	var wg sync.WaitGroup
+
 	for i := 1; i <= 100; i++ {
-		u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
-		download(u)
+		// goroutineの中で追加してしまうと、任意のタイミングで起動するgoroutineが起動する前にメインのgoroutineが
+		//最後に到達してしまい、処理が終わってしまう。なので、goroutineに入る前に値を追加する。
+		wg.Add(1)
+		i := i
+		go func() {
+			limit <- struct{}{}
+			defer wg.Done()
+			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
+			download(u)
+			<-limit
+		}()
 	}
+	wg.Wait()
+	//処理時間計測
 	fmt.Printf("%v\n", time.Since(before))
 }
